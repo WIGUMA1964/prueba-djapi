c4411b18af34fbfc99b846c27e19956336c6137a
William <william.gutierrez@sena.edu.co>
2021-02-22 18:56:32 -0500

    Config Heroku

---
+++ api/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.conf import settings
+
+class OwnerModel(models.Model):
+    owner=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract=True
+
+# Create your models here.
+#class Categoria(models.Model):
+class Categoria(OwnerModel):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Categoría',
+        unique=True
+    )
+ 
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+ 
+    class Meta:
+        verbose_name_plural = "Categorías"
+ 
+ 
+#class SubCategoria(models.Model):
+class SubCategoria(OwnerModel):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Sub Categoría'
+    )
+ 
+    def __str__(self):
+        return '{}:{}'.format(self.categoria.descripcion,self.descripcion)
+ 
+    class Meta:
+        verbose_name_plural = "Sub Categorías"
+        unique_together = ('categoria','descripcion')
+ 
+#class Producto(models.Model):
+class Producto(OwnerModel):
+    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción del Producto',
+        unique=True
+    )
+    fecha_creado = models.DateTimeField()
+    vendido = models.BooleanField(default=False)
+ 
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+ 
+    class Meta:
+        verbose_name_plural = "Productos"
+
+
+
+ 
